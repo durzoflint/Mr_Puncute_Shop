ecbb6a1b5aad9a2373188b9712dc64e849fa16c9
durzoflint <abhinavu1201@gmail.com>
2018-09-25 18:30:30 +0530

    First Commit

---
+++ app/src/main/java/com/durzoflint/mrpuncture_shop/HomeActivity.java
@@ -0,0 +1,13 @@
+package com.durzoflint.mrpuncture_shop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
