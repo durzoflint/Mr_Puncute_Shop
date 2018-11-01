0c03759750e2cb19bd2a5e82f62b9a21e3c0c3ac
durzoflint <abhinavu1201@gmail.com>
2018-11-01 10:52:42 +0530

    Added Activities

--- app/src/main/java/com/durzoflint/mrpuncture_shop/HomeActivity.java
+++ app/src/main/java/com/durzoflint/mrpuncture_shop/HomeActivity.java
@@ -1,7 +1,10 @@
 package com.durzoflint.mrpuncture_shop;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        setTitle("On Going Orders");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.home_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.previous:
+                startActivity(new Intent(this, PreviousOrdersActivity.class));
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
